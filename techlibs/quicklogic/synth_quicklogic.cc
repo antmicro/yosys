523b1a07a6b37d61e00fae2f7f7039d3db1a2432
Lalit Sharma <lsharma@quicklogic.com>
2020-07-29 11:36:27 +0530

    Merging changes from parent repo

--- techlibs/quicklogic/synth_quicklogic.cc
+++ techlibs/quicklogic/synth_quicklogic.cc
@@ -24,73 +24,71 @@
 USING_YOSYS_NAMESPACE
 PRIVATE_NAMESPACE_BEGIN
 
-struct SynthQuicklogicPass : public ScriptPass
-{
-    SynthQuicklogicPass() : ScriptPass("synth_quicklogic", "synthesis for Quicklogic FPGAs") { }
+struct SynthQuickLogicPass : public ScriptPass {
 
-    void help() override
-    {
-        //   |---v---|---v---|---v---|---v---|---v---|---v---|---v---|---v---|---v---|---v---|
-        log("\n");
-        log("    synth_quicklogic [options]\n");
-        log("\n");
-        log("This command runs synthesis for Quicklogic FPGAs.\n");
-        log("\n");
-        log("    -top <module>\n");
-        log("        use the specified module as top module\n");
-        log("\n");
-        log("    -edif <file>\n");
-        log("        write the design to the specified EDIF file. writing of an output file\n");
-        log("        is omitted if this parameter is not specified.\n");
-        log("\n");
-        log("    -family <family>\n");
-        log("        run synthesis for the specified QuickLogic architecture\n");
-        log("        generate the synthesis netlist for the specified family.\n");
-        log("        supported values:\n");
-        log("        - pp3: PolarPro 3 \n");
-        log("        - ap3: ArcticPro 3 \n");
-        log("\n");
-        log("    -blif <file>\n");
-        log("        write the design to the specified BLIF file. writing of an output file\n");
-        log("        is omitted if this parameter is not specified.\n");
-        log("\n");
-        log("    -adder\n");
-        log("        use adder cells in output netlist\n");
-        log("\n");
-        log("The following commands are executed by this synthesis command:\n");
-        help_script();
-        log("\n");
-    }
+	SynthQuickLogicPass() : ScriptPass("synth_quicklogic", "Synthesis for QuickLogic FPGAs") {}
 
-    string top_opt, edif_file, blif_file, family, currmodule;
-    bool inferAdder;
+	void help() override
+	{
+		log("\n");
+		log("   synth_quicklogic [options]\n");
+		log("This command runs synthesis for QuickLogic FPGAs\n");
+		log("\n");
+		log("    -top <module>\n");
+		log("         use the specified module as top module\n");
+		log("\n");
+		log("    -family <family>\n");
+		log("        run synthesis for the specified QuickLogic architecture\n");
+		log("        generate the synthesis netlist for the specified family.\n");
+		log("        supported values:\n");
+		log("        - pp3: PolarPro 3 \n");
+		log("        - ap3: ArcticPro 3 \n");
+		log("\n");
+		log("    -edif <file>\n");
+		log("        write the design to the specified edif file. writing of an output file\n");
+		log("        is omitted if this parameter is not specified.\n");
+		log("\n");
+		log("    -blif <file>\n");
+		log("        write the design to the specified BLIF file. writing of an output file\n");
+		log("        is omitted if this parameter is not specified.\n");
+		log("\n");
+		log("    -adder\n");
+		log("        use adder cells in output netlist\n");
+		log("\n");
+		log("The following commands are executed by this synthesis command:\n");
+		help_script();
+		log("\n");
+	}
 
-    void clear_flags() override
-    {
-        top_opt = "-auto-top";
-        edif_file = "";
-        blif_file = "";
-        currmodule = "";
-        family = "pp3";
-        inferAdder = false;
-    }
+	std::string top_opt, edif_file, blif_file, family, currmodule;
+	bool inferAdder;
 
-    void execute(std::vector<std::string> args, RTLIL::Design *design) YS_OVERRIDE
-    {
-        string run_from, run_to;
-        clear_flags();
+	void clear_flags() override
+	{
+		top_opt = "-auto-top";
+		edif_file = "";
+		blif_file = "";
+		currmodule = "";
+		family = "pp3";
+		inferAdder = false;
+	}
 
-        size_t argidx;
-        for (argidx = 1; argidx < args.size(); argidx++)
-        {
-            if (args[argidx] == "-top" && argidx+1 < args.size()) {
-                top_opt = "-top " + args[++argidx];
-                continue;
-            }
-            if (args[argidx] == "-edif" && argidx+1 < args.size()) {
-                edif_file = args[++argidx];
-                continue;
-            }
+	void execute(std::vector<std::string> args, RTLIL::Design *design) YS_OVERRIDE
+	{
+		std::string run_from, run_to;
+		clear_flags();
+
+		size_t argidx;
+		for (argidx = 1; argidx < args.size(); argidx++)
+		{
+			if (args[argidx] == "-top" && argidx+1 < args.size()) {
+				top_opt = "-top " + args[++argidx];
+				continue;
+			}
+			if (args[argidx] == "-edif" && argidx+1 < args.size()) {
+				edif_file = args[++argidx];
+				continue;
+			}
             if (args[argidx] == "-family" && argidx+1 < args.size()) {
                 family = args[++argidx];
                 continue;
@@ -118,51 +116,51 @@ struct SynthQuicklogicPass : public ScriptPass
         log_pop();
     }
 
-    void script() override
-    {
-        if (check_label("begin"))
-        {
-            std::string readVelArgs = " +/quicklogic/" + family + "_cells_sim.v";
-            run("read_verilog -lib +/quicklogic/cells_sim.v" + readVelArgs);
-            run(stringf("hierarchy -check %s", help_mode ? "-top <top>" : top_opt.c_str()));
-        }
+	void script() override
+	{
+		if (check_label("begin"))
+		{
+            std::string readVelArgs = "+/quicklogic/" + family + "_cells_sim.v";
+            run("read_verilog -lib -specify " + readVelArgs);
 
-        if (check_label("flatten"))
-        {
-            run("proc");
-            run("flatten");
-        }
+            run("read_verilog -lib +/quicklogic/cells_sim.v");
 
-        if (check_label("coarse"))
-        {
-            run("tribuf -logic");
-            run("deminout");
-            run("opt_expr");
-            run("opt_clean");
-            run("check");
-            run("opt");
+			run(stringf("hierarchy -check %s", help_mode ? "-top <top>" : top_opt.c_str()));
+		}
+
+		if (check_label("flatten"))
+		{
+			run("proc");
+			run("flatten");
             run("wreduce -keepdc");
-            run("peepopt");
-            run("opt_clean");
-            run("alumacc");
-            run("share");
-            run("opt");
-            run("fsm");
-            run("opt -fast");
+            run("muxpack");
+			run("tribuf -logic");
+			run("deminout");
+			run("opt");
+			run("opt_clean");
+			run("peepopt");
+		}
 
-            run("memory -nomap");
-            run("opt_clean");
-        }
+		if (check_label("coarse"))
+		{
+			run("synth -run coarse");
+			/*run("opt");
+			run("opt_clean");
+			run("peepopt");
+			run("techmap");
+			run("opt");
+			run("check");*/
+		}
 
-        if (check_label("map_ffram"))
-        {
-            run("opt -fast -mux_undef -undriven -fine");
-            run("memory_map");
-            run("opt -undriven -fine");
-        }
+		if (check_label("map_ffram"))
+		{
+			run("opt -fast -mux_undef -undriven -fine");
+			run("memory_map");
+			run("opt -undriven -fine");
+		}
 
-        if (check_label("map_gates"))
-        {
+		if (check_label("map_gates"))
+		{
             if (inferAdder)
             {
 		    	run("techmap -map +/quicklogic/" + family + "_arith_map.v");
@@ -204,47 +202,50 @@ struct SynthQuicklogicPass : public ScriptPass
         {
             std::string techMapArgs = " -map +/quicklogic/cells_map.v";
             techMapArgs += " -map +/quicklogic/" + family + "_cells_map.v";
-            run("techmap" + techMapArgs);
-            run("clean");
-        }
-
-        if (check_label("check"))
-        {
-            run("hierarchy -check");
-            run("stat");
-            run("check -noinit");
-        }
-
-        if (check_label("iomap")) {
-            if (family == "pp3") {
-                run("clkbufmap -buf $_BUF_ Y:A -inpad ckpad Q:P");
-                run("iopadmap -bits -outpad outpad A:P -inpad inpad Q:P -tinoutpad bipad EN:Q:A:P A:top");
-            } else if (family == "ap3") {
-                run("clkbufmap -buf $_BUF_ Y:A -inpad ck_buff Q:A");
-                run("iopadmap -bits -outpad out_buff A:Q -inpad in_buff Q:A -toutpad EN:A:Q A:top");
-            }
-        }
-
-        if (check_label("finalize")) {
-            run("splitnets -ports -format ()");
-            run("setundef -zero -params -undriven");
-            run("hilomap -hicell logic_1 a -locell logic_0 a -singleton A:top");
+			run("techmap" + techMapArgs);
             run("opt_clean");
             run("check");
-        }
+            run("autoname");
+			run("clean");
+		}
 
-        if (check_label("edif"))
-        {
-            if (!edif_file.empty() || help_mode)
-                run(stringf("write_edif -nogndvcc -attrprop -pvector par %s %s", this->currmodule.c_str(), edif_file.c_str()));
-        }
+		if (check_label("check"))
+		{
+			run("hierarchy -check");
+			run("stat");
+			run("check -noinit");
+		}
 
-        if (check_label("blif"))
-        {
-            if (!blif_file.empty() || help_mode)
-                run(stringf("write_blif %s %s", top_opt.c_str(), blif_file.c_str()));
-        }
-    }
+		if (check_label("iomap")) {
+			if (family == "pp3") {
+				run("clkbufmap -buf $_BUF_ Y:A -inpad ckpad Q:P");
+				run("iopadmap -bits -outpad outpad A:P -inpad inpad Q:P -tinoutpad bipad EN:Q:A:P A:top");
+			} else if (family == "ap3") {
+				run("clkbufmap -buf $_BUF_ Y:A -inpad ck_buff Q:A");
+				run("iopadmap -bits -outpad out_buff A:Q -inpad in_buff Q:A -toutpad EN:A:Q A:top");
+			}
+		}
+
+		if (check_label("finalize")) {
+			run("splitnets -ports -format ()");
+			run("setundef -zero -params -undriven");
+			run("hilomap -hicell logic_1 a -locell logic_0 a -singleton A:top");
+			run("opt_clean");
+			run("check");
+		}
+
+		if (check_label("edif"))
+		{
+			if (!edif_file.empty() || help_mode)
+				run(stringf("write_edif -nogndvcc -attrprop -pvector par %s %s", this->currmodule.c_str(), edif_file.c_str()));
+		}
+
+		if (check_label("blif"))
+		{
+			if (!blif_file.empty() || help_mode)
+				run(stringf("write_blif %s %s", top_opt.c_str(), blif_file.c_str()));
+		}
+	}
 } SynthQuicklogicPass;
 
 PRIVATE_NAMESPACE_END
