1b3212e63bab6744d2b9947097a8aa6b9cd0d98f
Robert Winkler <rwinkler@antmicro.com>
2020-10-12 14:10:04 +0200

    ql: Format verilog files
    Signed-off-by: Robert Winkler <rwinkler@antmicro.com>

--- techlibs/quicklogic/ap2_io_map.v
+++ techlibs/quicklogic/ap2_io_map.v
@@ -1,22 +1,34 @@
-module \$__out_buff (Q, A);
-	output Q;
-    input A;
+module \$__out_buff (
+  Q, A
+);
+  output Q;
+  input A;
 
-    parameter _TECHMAP_CONSTMSK_A_ = 0;
-    parameter _TECHMAP_CONSTVAL_A_ = 0;
+  parameter _TECHMAP_CONSTMSK_A_ = 0;
+  parameter _TECHMAP_CONSTVAL_A_ = 0;
 
-    if(_TECHMAP_CONSTMSK_A_ == 1) begin
-        d_buff _TECHMAP_REPLACE_ (.OUT_DBUF(Q), .IN_DBUF(_TECHMAP_CONSTVAL_A_));
-    end
-    else begin
-        out_buff _TECHMAP_REPLACE_ (.Q(Q), .A(A));
-    end
+  if (_TECHMAP_CONSTMSK_A_ == 1) begin
+    d_buff _TECHMAP_REPLACE_ (
+      .OUT_DBUF(Q),
+      .IN_DBUF(_TECHMAP_CONSTVAL_A_)
+    );
+  end else begin
+    out_buff _TECHMAP_REPLACE_ (
+      .Q(Q),
+      .A(A)
+    );
+  end
 endmodule
 
-module \$__in_buff (Q, A);
-	output Q;
-    input A;
+module \$__in_buff (
+  Q, A
+);
+  output Q;
+  input A;
 
-    in_buff _TECHMAP_REPLACE_ (.Q(Q), .A(A));
+  in_buff _TECHMAP_REPLACE_ (
+    .Q(Q),
+    .A(A)
+  );
 
 endmodule
--- techlibs/quicklogic/ap3_io_map.v
+++ techlibs/quicklogic/ap3_io_map.v
@@ -1,22 +1,31 @@
-module \$__out_buff (Q, A);
-	output Q;
-    input A;
+module \$__out_buff (
+  Q, A
+);
+  output Q;
+  input A;
 
-    parameter _TECHMAP_CONSTMSK_A_ = 0;
-    parameter _TECHMAP_CONSTVAL_A_ = 0;
+  parameter _TECHMAP_CONSTMSK_A_ = 0;
+  parameter _TECHMAP_CONSTVAL_A_ = 0;
 
-    if(_TECHMAP_CONSTMSK_A_ == 1) begin
-        d_buff #(.DSEL(_TECHMAP_CONSTVAL_A_)) _TECHMAP_REPLACE_ (.Q(Q));
-    end
-    else begin
-        out_buff _TECHMAP_REPLACE_ (.Q(Q), .A(A));
-    end
+  if (_TECHMAP_CONSTMSK_A_ == 1) begin
+    d_buff #(.DSEL(_TECHMAP_CONSTVAL_A_)) _TECHMAP_REPLACE_ (.Q(Q));
+  end else begin
+    out_buff _TECHMAP_REPLACE_ (
+      .Q(Q),
+      .A(A)
+    );
+  end
 endmodule
 
-module \$__in_buff (Q, A);
-	output Q;
-    input A;
+module \$__in_buff (
+  Q, A
+);
+  output Q;
+  input A;
 
-    in_buff _TECHMAP_REPLACE_ (.Q(Q), .A(A));
+  in_buff _TECHMAP_REPLACE_ (
+    .Q(Q),
+    .A(A)
+  );
 
 endmodule
