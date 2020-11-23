6c5a2930556118aa8e5af3fc720cccbe451de9b4
Lalit Sharma <lsharma@quicklogic.com>
2020-11-23 01:34:39 -0800

    Not inferring io buffer in case of openfpga flow

--- techlibs/quicklogic/synth_quicklogic.cc
+++ techlibs/quicklogic/synth_quicklogic.cc
@@ -314,10 +314,8 @@ struct SynthQuickLogicPass : public ScriptPass {
                 run("clkbufmap -buf $_BUF_ Y:A -inpad ckpad Q:P");
                 run("iopadmap -bits -outpad outpad A:P -inpad inpad Q:P -tinoutpad bipad EN:Q:A:P A:top");
             } else {
-                run("clkbufmap -buf $_BUF_ Y:A -inpad ck_buff Q:A");
-                if (openfpga) {
-                    run("iopadmap -bits -outpad outpad A:Q -inpad inpad Q:A");
-                } else {
+                if (!openfpga) {
+                    run("clkbufmap -buf $_BUF_ Y:A -inpad ck_buff Q:A");
                     run("iopadmap -bits -outpad $__out_buff A:Q -inpad $__in_buff Q:A");
                     std::string techMapArgs = " -map +/quicklogic/" + family + "_io_map.v -autoproc";
                     run("techmap" + techMapArgs);
