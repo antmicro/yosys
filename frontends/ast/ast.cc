ba8d01ce97e1ba8edad8e4ab6c12b046c110de8d
Krzysztof Bieganski <kbieganski@antmicro.com>
2020-12-22 12:05:58 +0100

    Remove unused variable

--- frontends/ast/ast.cc
+++ frontends/ast/ast.cc
@@ -1558,7 +1558,6 @@ void AstModule::expand_interfaces(RTLIL::Design *design, const dict<RTLIL::IdStr
 							RTLIL::Module *intfmodule = design->module(interface_type); // All interfaces should at this point in time (assuming
 							                                                              // reprocess_module is called from the hierarchy pass) be
 							                                                              // present in design->modules_
-							AstModule *ast_module_of_interface = (AstModule*)intfmodule;
 							std::string interface_modport_compare_str = "\\" + interface_modport;
 							AstNode *modport = ast->find_child(AST::AST_MODPORT, interface_modport_compare_str); // modport == NULL if no modport
 							// Iterate over all wires in the interface and add them to the module:
