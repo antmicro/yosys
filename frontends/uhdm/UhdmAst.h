8a724d1dc21bd1fcfa4a380443304836d77cd4ee
Krzysztof Bieganski <kbieganski@antmicro.com>
2021-01-05 14:12:15 +0100

    Refactor to make code simpler Also improve error reporting

--- frontends/uhdm/UhdmAst.h
+++ frontends/uhdm/UhdmAst.h
@@ -6,7 +6,6 @@
 #undef cover
 
 #include "uhdm.h"
-#include "astnodelist.h"
 #include "uhdmastshared.h"
 
 YOSYS_NAMESPACE_BEGIN
@@ -16,100 +15,115 @@ class UhdmAst {
 		// Walks through one-to-many relationships from given parent
 		// node through the VPI interface, visiting child nodes belonging to
 		// ChildrenNodeTypes that are present in the given object.
-		void visit_one_to_many(const std::vector<int> childrenNodeTypes,
-				       vpiHandle parentHandle, AstNodeList parent,
+		void visit_one_to_many(const std::vector<int> child_node_types,
+				       vpiHandle parent_handle,
 				       const std::function<void(AST::AstNode*)>& f);
 
 		// Walks through one-to-one relationships from given parent
 		// node through the VPI interface, visiting child nodes belonging to
 		// ChildrenNodeTypes that are present in the given object.
-		void visit_one_to_one(const std::vector<int> childrenNodeTypes,
-				      vpiHandle parentHandle, AstNodeList parent,
+		void visit_one_to_one(const std::vector<int> child_node_types,
+				      vpiHandle parent_handle,
 				      const std::function<void(AST::AstNode*)>& f);
 
 		// Visit children of type vpiRange that belong to the given parent node.
-		void visit_range(vpiHandle obj_h, AstNodeList parent,
-				const std::function<void(AST::AstNode*)> &f);
+		void visit_range(vpiHandle obj_h, const std::function<void(AST::AstNode*)> &f);
 
 		// Visit the default expression assigned to a variable.
-		void visit_default_expr(vpiHandle obj_h, AstNodeList parent);
+		void visit_default_expr(vpiHandle obj_h);
 
 		// Create an AstNode of the specified type with metadata extracted from
 		// the given vpiHandle.
 		AST::AstNode* make_ast_node(AST::AstNodeType type, vpiHandle obj_h);
 
 		// Makes the passed node a cell node of the specified type
-		void make_cell(vpiHandle obj_h, AST::AstNode* node, AST::AstNode* type, AstNodeList& parent);
+		void make_cell(vpiHandle obj_h, AST::AstNode* node, AST::AstNode* type);
 
 		// Adds a typedef node to the current node
 		void add_typedef(AST::AstNode* current_node, AST::AstNode* type_node);
 
+		// Go up the UhdmAst to find a parent node of the specified type
+		AST::AstNode* find_ancestor(const std::unordered_set<AST::AstNodeType>& types);
+
 		// Reports that something went wrong with reading the UHDM file
 		void report_error(const char *format, ...) const;
+
+		// Processes the value connected to the specified node
+		AST::AstNode* process_value(vpiHandle obj_h);
+
+		// The parent UhdmAst
+		UhdmAst* parent;
 
 		// Data shared between all UhdmAst objects
 		UhdmAstShared& shared;
 
-		// Functions that handle specific types of nodes
-		AST::AstNode* handle_design(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_parameter(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_port(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_module(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_struct_typespec(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_typespec_member(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_enum_typespec(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_enum_const(vpiHandle obj_h);
-		AST::AstNode* handle_custom_var(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_int_var(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_array_var(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_param_assign(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_cont_assign(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_assignment(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_net(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_packed_array_net(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_array_net(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_package(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_interface(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_modport(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_io_decl(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_always(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_event_control(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_initial(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_begin(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_operation(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_list_op(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_cast_op(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_inside_op(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_assignment_pattern_op(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_tagged_pattern(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_bit_select(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_part_select(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_indexed_part_select(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_var_select(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_if_else(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_for(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_gen_scope_array(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_gen_scope(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_case(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_case_item(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_constant(vpiHandle obj_h);
-		AST::AstNode* handle_range(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_return(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_function(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_logic_var(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_sys_func_call(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_func_call(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_immediate_assert(vpiHandle obj_h, AstNodeList& parent);
-		AST::AstNode* handle_hier_path(vpiHandle obj_h, AstNodeList& parent);
+		// The current VPI/UHDM handle
+		vpiHandle obj_h = 0;
+
+		// The current Yosys AST node
+		AST::AstNode* current_node = nullptr;
 
 		// Indentation used for debug printing
 		std::string indent;
 
+		// Functions that process specific types of nodes
+		void process_design();
+		void process_parameter();
+		void process_port();
+		void process_module();
+		void process_struct_typespec();
+		void process_typespec_member();
+		void process_enum_typespec();
+		void process_enum_const();
+		void process_custom_var();
+		void process_int_var();
+		void process_array_var();
+		void process_param_assign();
+		void process_cont_assign();
+		void process_assignment();
+		void process_net();
+		void process_packed_array_net();
+		void process_array_net();
+		void process_package();
+		void process_interface();
+		void process_modport();
+		void process_io_decl();
+		void process_always();
+		void process_event_control();
+		void process_initial();
+		void process_begin();
+		void process_operation();
+		void process_list_op();
+		void process_cast_op();
+		void process_inside_op();
+		void process_assignment_pattern_op();
+		void process_tagged_pattern();
+		void process_bit_select();
+		void process_part_select();
+		void process_indexed_part_select();
+		void process_var_select();
+		void process_if_else();
+		void process_for();
+		void process_gen_scope_array();
+		void process_gen_scope();
+		void process_case();
+		void process_case_item();
+		void process_range();
+		void process_return();
+		void process_function();
+		void process_logic_var();
+		void process_sys_func_call();
+		void process_func_call();
+		void process_immediate_assert();
+		void process_hier_path();
+
+		UhdmAst(UhdmAst* p, UhdmAstShared& s, const std::string& i) : parent(p), shared(s), indent(i) {}
+
 	public:
-		UhdmAst(UhdmAstShared& s, const std::string& i = "") : shared(s), indent(i) {}
+		UhdmAst(UhdmAstShared& s, const std::string& i = "") : UhdmAst(nullptr, s, i) {}
 
 		// Visits single VPI object and creates proper AST node
-		AST::AstNode* handle_object(vpiHandle obj_h, AstNodeList parent = AstNodeList());
+		AST::AstNode* process_object(vpiHandle obj_h);
 
 		// Visits all VPI design objects and returns created ASTs
 		AST::AstNode* visit_designs(const std::vector<vpiHandle>& designs);
