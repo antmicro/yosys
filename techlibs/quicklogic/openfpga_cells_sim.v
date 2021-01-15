42344d3a69b6c930b7688bab43a2f2b4f9a983f1
Lalit Sharma <lsharma@quicklogic.com>
2021-01-15 17:27:31 +0530

    Updating soft_adder synthesis support for openfpga device

--- techlibs/quicklogic/openfpga_cells_sim.v
+++ techlibs/quicklogic/openfpga_cells_sim.v
@@ -1,23 +1,37 @@
-
+(* lib_blackbox *)
 module carry_follower(
-	output CO,
-	input A,
-	input B,
-	input CI
+    input a,
+    input b,
+	input cin,
+	output cout
 );
-	assign CO = ((A ^ B) & CI) | (~(A ^ B) & (A & B)); 
+	//assign cout = ((a ^ b) & cin) | (~(a ^ b) & (a & b)); 
 endmodule
 
-(* abc9_lut=1, lib_whitebox *)
-module LUT4(
-   output O, 
-   input I0,
-   input I1,
-   input I2,
-   input I3
+(* abc9_lut=1, lib_blackbox *)
+module frac_lut4(
+   input [3:0] in,
+   output [1:0] lut2_out,
+   output lut4_out
 );
-    parameter [15:0] INIT = 16'h0;
-    parameter EQN = "(I0)";
+    parameter [15:0] LUT = 16'h0;
     
-    assign O = INIT[{I3, I2, I1, I0}];
+    // Effective LUT input
+    wire [3:0] li = in;
+
+    // Output function
+    wire [7:0] s1 = li[0] ?
+        {LUT[15], LUT[13], LUT[11], LUT[9], LUT[7], LUT[5], LUT[3], LUT[1]} :
+        {LUT[14], LUT[12], LUT[10], LUT[8], LUT[6], LUT[4], LUT[2], LUT[0]};
+
+    wire [3:0] s2 = li[1] ? {s1[7], s1[5], s1[3], s1[1]} :
+                            {s1[6], s1[4], s1[2], s1[0]};
+
+    wire [1:0] s3 = li[2] ? {s2[3], s2[1]} : {s2[2], s2[0]};
+
+    assign lut2_out[0] = s2[0];
+    assign lut2_out[1] = s2[1];
+
+    assign  lut4_out = li[3] ? s3[1] : s3[0];
+
 endmodule
