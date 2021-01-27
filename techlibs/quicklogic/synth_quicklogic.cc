a638be3fdf7d8e586e69b0ec52f88aa9df1e5148
Lalit Sharma <lsharma@quicklogic.com>
2021-01-27 12:55:16 +0530

    Updating code to convert async to sync during synthesis in openfpga flow

--- techlibs/quicklogic/synth_quicklogic.cc
+++ techlibs/quicklogic/synth_quicklogic.cc
@@ -158,13 +158,10 @@ struct SynthQuickLogicPass : public ScriptPass {
                 readVelArgs = " +/quicklogic/" + family + "_cells_sim.v";
             }
             run("read_verilog -lib -specify +/quicklogic/cells_sim.v" + readVelArgs);
-            if(openfpga) {
-                //run("read_verilog -lib -specify +/quicklogic/abc9_softadder_model.v");
-            }
             run(stringf("hierarchy -check %s", help_mode ? "-top <top>" : top_opt.c_str()));
         }
 
-        if (check_label("prepare") && (!openfpga || (openfpga && inferAdder)))  {
+        if (check_label("prepare"))  {
             run("proc");
             run("flatten");
             if(family == "pp3" || family == "ap") {
@@ -176,7 +173,7 @@ struct SynthQuickLogicPass : public ScriptPass {
             run("opt");
         }
 
-        if (check_label("coarse") && (!openfpga || (openfpga && inferAdder))) {
+        if (check_label("coarse")) {
             run("opt_expr");
             run("opt_clean");
             run("check");
@@ -198,13 +195,6 @@ struct SynthQuickLogicPass : public ScriptPass {
             run("opt_clean");
         }
 
-        if(openfpga && !inferAdder) {
-            run("proc");
-            run("techmap  -D NO_LUT -map +/adff2dff.v");
-            run("synth " + top_opt + " -flatten");
-            run("clean");
-        }
-
         if (check_label("map_bram", "(skip if -nobram)") && family == "pp3") {
             run("memory_bram -rules +/quicklogic/" + family + "_brams.txt");
             run("pp3_braminit");
@@ -220,9 +210,11 @@ struct SynthQuickLogicPass : public ScriptPass {
         }
 
         if (check_label("map_gates")) {
+            if(openfpga) {
+                run("async2sync");
+            }
             if (inferAdder && family != "pp3" && family != "ap") {
                 if(openfpga) {
-                    //run("ql_wrapcarry");
                     run("techmap -map +/techmap.v -map +/quicklogic/openfpga_arith_map.v");
                 } else {
                     run("techmap -map +/techmap.v -map +/quicklogic/" + family + "_arith_map.v");
@@ -301,10 +293,6 @@ struct SynthQuickLogicPass : public ScriptPass {
                 run("abc -lut 4 ");
             }
 
-            //if(family != "pp3") {
-            //run("ap3_wrapcarry -unwrap");
-            //}
-
             techMapArgs = " -map +/quicklogic/" + family + "_ffs_map.v";
             if(!openfpga) {
                 run("techmap " + techMapArgs);
@@ -312,7 +300,6 @@ struct SynthQuickLogicPass : public ScriptPass {
 
             run("clean");
             if(family != "pp3" && family != "ap") {
-                //run("opt_lut -dlogic QL_CARRY:I0=2:I1=1:CI=0");
                 run("opt_lut");
             }
         }
