92499f016d8643a0c44718cb6399e0c026d7405b
Kamil Rakoczy <krakoczy@antmicro.com>
2021-02-04 08:37:21 +0100

    Add building sv2v
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

--- build_binaries.sh
+++ build_binaries.sh
@@ -5,3 +5,5 @@ make -C vcddiff PREFIX=./image
 cp -p vcddiff/vcddiff image/bin/vcddiff
 cd verilator/Surelog && make PREFIX=$PWD/../image/ release install -j $(nproc) && cd ..
 autoconf && ./configure --prefix=$PWD/../image && make install && cd ..
+wget -qO- https://get.haskellstack.org/ | sh -s - -d $PWD/image/bin
+export PATH=$PWD/image/bin:${PATH} && make -C $PWD/sv2v && cp $PWD/sv2v/bin/sv2v $PWD/image/bin
