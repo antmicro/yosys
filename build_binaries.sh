b6d567a2d36f3f0d65dbbfa428550e359b3acd36
Kamil Rakoczy <krakoczy@antmicro.com>
2021-02-09 15:21:44 +0100

    Remove vcddiff submodule
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

--- build_binaries.sh
+++ build_binaries.sh
@@ -5,9 +5,6 @@ INSTALL_PATH=$PWD/image
 cd Surelog && make PREFIX=$INSTALL_PATH release install -j $(nproc) && cd ..
 #Yosys
 make PREFIX=$INSTALL_PATH install -j $(nproc)
-#vcddiff
-make -C vcddiff PREFIX=$INSTALL_PATH -j $(nproc)
-cp -p vcddiff/vcddiff $INSTALL_PATH/bin/vcddiff
 #sv2v
 wget -qO- https://get.haskellstack.org/ | sh -s - -d $INSTALL_PATH/bin
 export PATH=$INSTALL_PATH/bin:${PATH} && make -C $PWD/sv2v && cp $PWD/sv2v/bin/sv2v $INSTALL_PATH/bin
