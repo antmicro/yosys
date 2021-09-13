4bdddc4ded8c4980940a4657947ed57557cea986
pawelsag <psagan@antmicro.com>
2021-09-13 12:31:58 +0200

    Update frontends/ast/simplify.cc
    Co-authored-by: Kamil Rakoczy <krakoczy@antmicro.com>

--- frontends/ast/simplify.cc
+++ frontends/ast/simplify.cc
@@ -570,9 +570,8 @@ static void flatten_ranges(AstNode *node)
 		return;
 	}
 
-	unsigned ranges = std::count_if(node->children.begin(),
-			node->children.end(),
-			[](AstNode *n){return n->type == AST_RANGE;});
+	unsigned ranges = std::count_if(node->children.begin(), node->children.end(),
+			[](AstNode *n) { return n->type == AST_RANGE; });
 
 	if (ranges <= 1) {
 		return;
