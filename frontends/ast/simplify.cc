11a2096d795d3237a7e467893bbd88f72640453e
Pawel Sagan <psagan@antmicro.com>
2021-09-16 13:12:13 +0200

    adding code to handle packed structures copy and tests for them

--- frontends/ast/simplify.cc
+++ frontends/ast/simplify.cc
@@ -1509,8 +1509,20 @@ bool AstNode::simplify(bool const_fold, bool at_zero, bool in_lvalue, int stage,
 			while (template_node->simplify(const_fold, at_zero, in_lvalue, stage, width_hint, sign_hint, in_param)) {};
 
 			if (template_node->type == AST_STRUCT || template_node->type == AST_UNION) {
-				// replace with wire representing the packed structure
 				newNode = make_packed_struct(template_node, str);
+				if (children.size() == 2 && children[1]->type == AST_RANGE) {
+					newNode->attributes[ID::wiretype] = mkconst_str(resolved_type_node->str);
+					newNode->attributes[ID::wiretype]->children.push_back(children[1]->clone()); // save unpacked size
+					newNode->attributes[ID::wiretype]->is_packed = true;
+					if(port_id == 0 && type == AST_WIRE) {
+						int s = std::abs(int(children[1]->children[0]->integer - children[1]->children[1]->integer)) + 1;
+						newNode->children[0]->range_left = (newNode->children[0]->range_left + 1) * s;
+						newNode->children[0]->children[0]->integer = (newNode->children[0]->children[0]->integer + 1) * s;
+						newNode->children[0]->range_left -= 1;
+						newNode->children[0]->children[0]->integer -= 1;
+					}
+				}
+				// replace with wire representing the packed structure
 				newNode->is_input = this->is_input;
 				newNode->is_output = this->is_output;
 				current_scope[str] = this;
@@ -1851,6 +1863,27 @@ bool AstNode::simplify(bool const_fold, bool at_zero, bool in_lvalue, int stage,
 							did_something = true;
 						}
 					}
+				}
+			}
+		}
+	}
+
+	if(type == AST_IDENTIFIER && current_scope.count(str) && current_scope[str]->attributes.count(ID::wiretype))
+	{
+		auto wiretype = current_scope[str]->attributes[ID::wiretype];
+		if(current_scope.count(wiretype->str))
+		{
+			auto orig_struct = current_scope[wiretype->str];
+			if(orig_struct->children.size() &&
+				(orig_struct->children[0]->type == AST_STRUCT ||
+				 orig_struct->children[0]->type == AST_UNION)){
+
+				auto width = size_packed_struct(orig_struct, 0);
+
+				if(children.size() && children[0]->range_left == children[0]->range_right)
+				{
+					children[0]->range_left = width*(children[0]->range_left+1)-1;
+					children[0]->range_right = width*children[0]->range_right;
 				}
 			}
 		}
--- tests/various/multirange_3.sv
+++ tests/various/multirange_3.sv
@@ -1,14 +1,67 @@
+typedef struct packed {
+  logic a_p;
+  logic a_n;
+} pinout_t;
+
+
+typedef struct packed {
+   logic [1:0] s1;
+   logic [2:0] s2;
+   logic       s3;
+} parity_t;
+
+typedef struct packed {
+   logic                  [2:0]   var1;
+   logic                  [2:0]   var2;
+   parity_t                       var3;
+} complex_struct_t;
+
+parameter int Size = 4;
+
 module dut();
+
 logic [3:0][3:0][7:0] multirange_logic;
 logic [15:0] multirange_logic_cpy;
+
+pinout_t [Size*2-1:0] pinout = 8'b10110010;
+logic [2:0] pinout_part_cpy;
+
+complex_struct_t complex_hw [3];
+complex_struct_t complex_hw_cpy;
+
+localparam int N_HARTS  = 2;
+localparam int N_TIMERS = 1;
+logic [3:0] mtime            [N_HARTS];
+logic [3:0] mtimecmp         [N_HARTS][N_TIMERS];
+logic       mtimecmp_update  [1][1];
 
 assign multirange_logic[3][3:2] = 16'b1010101010101010;
 assign multirange_logic[3][1:0] = 16'b0000000000000000;
 
 assign multirange_logic_cpy = multirange_logic[3][2:1];
 
+assign pinout_part_cpy = pinout[4:2];
+
+assign complex_hw[1] = 12'b110010011100;
+assign complex_hw_cpy = complex_hw[1];
+
+assign mtime[0] = 4'b1100;
+assign mtime[1] = 4'b0011;
+
+assign mtimecmp_update[0][0] = 1;
+
+assign mtimecmp[0][0][3:2] = mtime[0][3:2];
+assign mtimecmp[1][0][1:0] = mtime[1][1:0];
+
 always_comb begin
   assert(multirange_logic_cpy == 16'b1010101000000000);
+
+  assert(pinout_part_cpy == 3'b100);
+
+  assert(complex_hw_cpy == 12'b110010011100);
+
+  assert(mtimecmp[0][0][3:2] == mtimecmp[1][0][1:0]);
+  assert(mtimecmp_update[0][0] == 1);
 end
 
 endmodule
